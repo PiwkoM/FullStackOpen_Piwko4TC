f1cba238065f2a747399fbf9017cf194e2eeb57a
PiwkoM <piwkom44@gmail.com>
2024-10-21 14:03:29 +0200

    1.10

--- part_1/src/App.jsx
+++ part_1/src/App.jsx
@@ -1,5 +1,33 @@
 import { useState } from 'react'
 
+const StatisticLine = (props) => {
+  return(
+  <p>{props.text} {props.value}</p>
+  )
+}
+
+const Statisticss = (props) =>{
+  let tot = (props.good+props.neutral+props.bad)
+  if(tot ==0){
+    return(
+      <>
+      <p>No data found</p>
+      </>
+    )
+  }
+  return(
+      <div>
+      <StatisticLine text="good" value={props.good}/>
+      <StatisticLine text="neutral" value={props.neutral}/>
+      <StatisticLine text="bad" value={props.bad}/>
+      <StatisticLine text="total" value={tot}/>
+      <StatisticLine text="average" value={(props.good*1+props.bad*-1)/tot+'%'}/>
+      <StatisticLine text="positive" value={props.good*100/tot+'%'}/>
+      </div>
+  )
+  
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -15,10 +43,6 @@ const App = () => {
       setBad(bad+1)
   }
 
-
-
-
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -26,12 +50,13 @@ const App = () => {
       <button onClick={hanN}>neutral</button>
       <button onClick={hanM}>MORBIUS!!!!!</button>
       <h1>statistics</h1>
-      <p>gud {good}</p>
+      <Statisticss good={good} neutral={neutral} bad={bad}/>
+      {/* <p>gud {good}</p>
       <p>nutral {neutral}</p>
       <p>morbius {bad}</p>
       <p>total {(good+neutral+bad)}</p>
       <p>average {(good-bad)/(good+neutral+bad)}</p>
-      <p>positive {good/(good+neutral+bad)}%</p>
+      <p>positive {good/(good+neutral+bad)}%</p> */}
     </div>
   )
 }
