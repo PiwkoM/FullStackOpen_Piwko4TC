a59fbc6f7abc0c383abfe53d47e5391e54f77a60
PiwkoM <piwkom44@gmail.com>
2024-11-25 12:58:06 +0100

    2.6

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -6,23 +6,38 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
-  const addName = () =>{
-    setPersons(persons.concat())
+  const inputChange = (event) => {
+    setNewName(event.target.value)
+  }
+  
+  const addName = (e) =>{
+    e.preventDefault()
+    console.log(newName)
+    setPersons(persons.concat({name: newName}))
+  }
+
+  const Person = (p) =>{
+    <div>{p.name}</div>
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
+        {/*onSubmit={addName} */}
         <div>
-          name: <input />
+          name: <input value={newName} onChange={inputChange}/>
+          {/* name: <input/> */}
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(p => (<div key={p.name}>{p.name} </div>))}
+        {/* {persons.map(persons => (
+         <Person key={persons.name} p={persons.name}/>
+        ))} */}
     </div>
   )
 }
