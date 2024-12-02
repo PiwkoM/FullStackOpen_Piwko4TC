01fb1a05fe259e5b31157bab47edf5c7565e67a7
PiwkoM <piwkom44@gmail.com>
2024-12-02 12:17:27 +0100

    2.9

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "55-44-55-44" }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [finder, setFinder] = useState('')
 
   const inputChange = (event) => {
     setNewName(event.target.value)
@@ -14,27 +18,31 @@ const App = () => {
   const inputChange_num = (event) => {
     setNewNumber(event.target.value)
   }
-  
-  const addName = (e) =>{
-    e.preventDefault()
-    console.log(newName)
-    if(persons.some(person => person.name === newName) || persons.some(person => person.number === newNumber)){
-      alert(`${newName} or ${newNumber} is already added to phonebook`)
-    } else { setPersons(persons.concat({name: newName, number: newNumber})) }
-    setNewName('')
-    setNewNumber('')
-    {
-      /* 
-        .some() iterates through array and check if object meets requirement specified
-        after the '=>' symbol
-      */
-    }
+
+  const findName = (event) => {
+    setFinder(event.target.value)
   }
 
+  const addName = (e) => {
+    e.preventDefault()
+    if(persons.some(person => person.name === newName) || persons.some(person => person.number === newNumber)){
+      alert(`${newName} or ${newNumber} is already added to phonebook`)
+    } else { 
+      setPersons(persons.concat({name: newName, number: newNumber})) 
+    }
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(finder.toLowerCase())
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <input value={finder} onChange={findName} placeholder="Search by name" />
+      <h2>Add a new number </h2>
       <form onSubmit={addName}>
         <div>
           <div>name: <input value={newName} onChange={inputChange}/></div>
@@ -45,9 +53,14 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p => (<div key={p.name}>{p.name} {p.number} </div>))}
+      {filteredPersons.map(p => (
+        <div key={p.name}>{p.name} {p.number} </div>
+      ))}
     </div>
   )
 }
 
 export default App
+
+
+{/* {persons.map(p => (<div key={p.name}>{p.name} {p.number} </div>))} */}
