2f114385104d9956674b5db4f6b6285367bfb0d1
PiwkoM <piwkom44@gmail.com>
2025-04-25 13:47:22 +0200

    3.3 ! ! !

--- part3/index.js
+++ part3/index.js
@@ -32,6 +32,17 @@ app.get('/api/persons',(request,response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id; 
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    response.json(person); 
+  } else {
+    response.status(404).end();
+  }
+})
+
 app.get('/info',(request,response) => {
   const date = new Date();
   response.send(`
@@ -40,6 +51,8 @@ app.get('/info',(request,response) => {
   `)
 
 })
+
+
 
 // app.get('/api/persons/:id', (request, response) => {
 //   const id = request.params.id
